fc0fdf5aebc0bfaf69718c652c2c7f59cce93437
paulinemalova <perleebrown@gmail.com>
2019-04-16 19:50:11 +0300

    Re-adding my python class progress.

---
+++ Example 2/calculator.py
@@ -0,0 +1,39 @@
+def add(a,b):
+	c=a+b
+	print(c)
+
+
+def subtract(d,e):
+	f=d-e
+	print(f)
+
+
+def multiply(a,b):
+	c=a*b
+	print(c)
+
+
+def divide(d,e):
+	f=d/e
+	print(f)
+
+
+def modulus(a,b):
+	c=a%b
+	print(c)
+
+
+def whole_division(d,e):
+	f=d//e
+	print(f)
+
+def percentage(a,b):
+	c=(b/a)*100
+	print("{}%".format(c))
+
+
+def exponent(d,e):
+	f=d**e
+	print(f)
+
+
